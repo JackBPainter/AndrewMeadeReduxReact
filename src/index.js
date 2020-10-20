1c4573d08a0fd941739c93f045b3ea7cd0459947
Jack Painter <littlesympathy@jacks-mbp-2.home>
2020-10-20 08:47:42 +0100

    Wrap AppRouter in Provider and connext ExpenseList component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
@@ -13,16 +14,21 @@ store.dispatch(addExpense({ description: "water bill" }))
 store.dispatch(addExpense({ description: "gas bill" }))
 store.dispatch(setTextFilter('water'))
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'))
+}, 3000 )
+
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 
-ReactDOM.render(
-  <React.StrictMode>
+const jsx = (
+  <Provider store={store}>
     <AppRouter />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
