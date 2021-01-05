90b2d1f95ad15e2c2f6030e5537a25ab10545f23
Jack Painter <littlesympathy@jacks-mbp-2.home>
2021-01-05 09:59:55 +0000

    Create initial filters reducer tests

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -12,9 +12,9 @@ export default (state = filtersReducerDefaultState, action) => {
         case "SET_TEXT_FILTER":
             return {...state, text: action.text}
         case "SORT_BY_AMOUNT":
-            return {...state, sortBy: action.amount}
+            return {...state, sortBy: 'amount'}
         case "SORT_BY_DATE":
-            return {...state, sortBy: action.date} 
+            return {...state, sortBy: 'date'} 
         case "SET_START_DATE":
             return {...state, startDate: action.date}
         case "SET_END_DATE":
